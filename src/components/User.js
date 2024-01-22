f9f0b97e0ca0e5ec42e6456c7a696b8cf56c3e6b
Chakri341 <111366934+Chakri341@users.noreply.github.com>
2024-01-22 20:07:42 +0530

    Update User.js

--- src/components/User.js
+++ src/components/User.js
@@ -3,8 +3,8 @@ const User = ({ name }) => {
   return (
     <div className="user-card m-4 p-4 bg-gray-50 rounded-lg">
       <h2>Name: {name}</h2>
-      <h3>Location: Dehradun</h3>
-      <h4>Contact: @akshaymarch7</h4>
+      <h3>Location: Nellore</h3>
+      <h4>Contact:potlur_931943@student.nitw.ac.in</h4>
     </div>
   );
 };
